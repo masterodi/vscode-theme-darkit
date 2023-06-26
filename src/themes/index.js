8002c47a1d235858667a9781c7782490a68954da
mrheio <drg.ov00@gmail.com>
2023-06-26 20:57:06 +0300

    added new theme: Atomic

--- src/themes/index.js
+++ src/themes/index.js
@@ -1,3 +1,4 @@
 export { default as astral } from './astral.js';
+export { default as atomic } from './atomic.js';
 export { default as base } from './default.js';
 export { default as malakai } from './malakai.js';
